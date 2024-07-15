ae3df0d881802bf6b4795cf8d3901d055ae9351b
Fareed Elbokl <fareed.elbokl@gmail.com>
2024-07-15 16:18:07 -0400

    Added tests for user model and user model

--- server/app/app/settings.py
+++ server/app/app/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core'
+    'rest_framework.authtoken'
+    'core',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
